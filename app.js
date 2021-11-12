973f4443685565a816ac9c863531963650c5e5fd
Sidra ELEzz <76815358+SidraELEzz@users.noreply.github.com>
2021-11-12 14:44:42 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// @SidraTools
+const express = require("express");
+const path = require("path");
+const app = express();
+const { scrape } = require("./scraper.js");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("API"));
+app.use(cors({
+    origin: "*"
+}));
+
+app.get("/", async (_, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get("/api/:bin", async (req, res) => {
+    try {
+        let data = await scrape(req.params.bin);
+        res.set("Cache-Control", "API, max-age=86400");
+        res.type('application/json');
+        res.send(data);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
+app.use(async (_, res) => {
+    res.redirect(301, "/");
+});
+
+app.listen(PORT, () => console.log(`Server is running... [PORT : ${PORT}]`));
